7c9f78756ed21f482b68a9670984ab4d8d220852
kanishka0103 <kanishkas602@gmail.com>
2025-09-23 02:15:16 +0530

    updated corns

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const app = require('./src/app')
 
 
+const cors = require("cors");
+app.use(cors());
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
